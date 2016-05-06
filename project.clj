74b1d0feec98737ae35ba5f5cb80c81b3283d665
jbo <jbo@ableton.com>
2016-05-06 17:11:28 +0200

    Bump version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject arximboldi-static/lein-template "0.1.0-SNAPSHOT"
+(defproject arximboldi-static/lein-template "0.1.1"
   :description "Opinionated Leinigen template for the development of client-side
   web apps with ClojureScript, Reagent and friends."
   :url "https://github.com/arximboldi/arximboldi-static"
